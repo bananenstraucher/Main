a606b205371b8d94d48b74dada6249c3c2614472
bananenstraucher <100133032+bananenstraucher@users.noreply.github.com>
2023-08-20 17:38:05 +0200

    update

--- script.js
+++ script.js
@@ -33,42 +33,6 @@ function insertRepeatedContent(targetElementId) {
     </div>';
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    insertRepeatedContent('wiederholender-Content');
-    // Sie können die Funktion auch für andere Elemente aufrufen
-    // insertRepeatedContent('anotherElementId');
-});
-
-window.addEventListener('DOMContentLoaded', (event) => {
-    const gehashtesPasswort = "$2a$20$4ZSwgOC3ceni/z09QRBDnua0RHoHzpdF44pj4LD4Ec0g16SFmG1Jy"; 
-
-    // Funktion, um einen bestimmten Cookie zu erhalten
-    function getCookie(name) {
-        const value = `; ${document.cookie}`;
-        const parts = value.split(`; ${name}=`);
-        if (parts.length === 2) return parts.pop().split(';').shift();
-    }
-
-    // Überprüfen Sie, ob der Authentifizierungscookie gesetzt ist
-    const authCookie = getCookie("auth");
-
-    if (authCookie === "true") {
-        document.body.style.display = "block";
-    } else {
-        const urlParams = new URLSearchParams(window.location.search);
-        const passwParam = urlParams.get('passw');
-
-        if (bcrypt.compareSync(passwParam, gehashtesPasswort)) {
-            document.body.style.display = "block";
-            
-            // Setzen Sie den Authentifizierungscookie
-            document.cookie = "auth=true; max-age=2678400";  // Gültig für 1 Stunde (3600 Sekunden)
-        } else {
-            alert('Ungültiges Passwort');
-        }
-    }
-});
-
 const start = () => {
     // Initialize the JavaScript client library
     gapi.client.init({
