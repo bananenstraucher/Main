5ac2b8cc0379fc84ae0ad43f5953351021b8a313
bananenstraucher <100133032+bananenstraucher@users.noreply.github.com>
2023-09-29 20:48:23 +0200

    Add files via upload

---
+++ scipt_spruch.js
@@ -0,0 +1,50 @@
+async function holeSprueche() {
+    try {
+        const response = await fetch('sprueche.txt');
+        const text = await response.text();
+        return text.split(';').filter(eintrag => eintrag.split('|').length === 3).map(eintrag => {
+            const [spruch, iconText, link] = eintrag.split('|');
+            return { 
+                spruch: spruch.trim(), 
+                iconText: iconText.trim(),
+                link: link.trim() === "" ? "unbekannt" : link.trim()
+            };
+        });
+    } catch (error) {
+        console.error('Fehler beim Laden der Sprüche:', error);
+    }
+    return [];
+}
+
+async function neuerSpruch() {
+    const sprueche = await holeSprueche();
+    if (sprueche.length > 0) {
+        const zufallIndex = Math.floor(Math.random() * sprueche.length);
+        const ausgewaehlterSpruch = sprueche[zufallIndex];
+
+        document.getElementById('spruchAnzeige').innerText = ausgewaehlterSpruch.spruch;
+
+        // Update den Link und das Icon
+        const linkElement = document.getElementById('spruchLink');
+        const iconElement = document.getElementById('spruchIcon');
+    
+        if (ausgewaehlterSpruch.link !== "unbekannt") {
+            linkElement.href = ausgewaehlterSpruch.link;
+
+            // Setzen Sie das Icon-Bild basierend auf dem Icon-Text
+            iconElement.src = `icons_sprueche/${ausgewaehlterSpruch.iconText}.png`;
+            iconElement.alt = ausgewaehlterSpruch.iconText;
+
+        } else {
+            linkElement.href = "#";
+            iconElement.src = "";
+            iconElement.alt = "";
+        }
+    }
+}
+
+// Lädt einen Spruch, sobald die Seite geladen ist
+window.onload = neuerSpruch;
+
+
+
